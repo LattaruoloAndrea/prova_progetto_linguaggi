07b5f0ff479b4237c20524411996785b606c545d
VashTheStampede96 <brian.riccardi@gmail.com>
2020-05-20 22:39:20 +0200

    infer literals, predefined read, array initializer list

--- src/haskell/TypeChecker.hs
+++ src/haskell/TypeChecker.hs
@@ -6,23 +6,35 @@ import TCInstances
 import Env
 import qualified ErrM as EM
 
+import Control.Monad (when)
+
+-- Copied from Skel --------------------------------------
+type Result = EM.Err TCType
+
+failure :: Show a => a -> Result
+failure x = EM.Bad $ "Undefined case: " ++ show x
+----------------------------------------------------------
 
 
--- inferRExp :: RExp -> Env -> EM.Err TCType
--- inferRExp rexp env = case rexp of
---     Or rexp1 rexp2 -> if (inferRExp rexp1 env) `compare'` (inferRExp rexp2 env) == TNotComparable
---         then TError         -- Add error msg
---         else TError
-         
---     And rexp1 rexp2 -> inferLogical rexp1 rexp2 env
+inferRExp :: Env -> RExp -> EM.Err TCType
+inferRExp env rexp = let x = "Dummy" in case rexp of
+    Or rexp1 rexp2 -> failure x
+    And rexp1 rexp2 -> failure x
+    Not rexp -> failure x
+    Comp rexp1 compop rexp2 -> failure x
+    Arith rexp1 arithop rexp2 -> failure x
+    Sign signop rexp -> failure x
+    RefE lexp -> failure x
+    RLExp lexp -> failure x
+    
+    ArrList rexps -> case rexps of
+        [] -> EM.Bad "Error: empty array initializer."
+        _  -> do
+            t <- foldl1 supremumM $ map (inferRExp env) rexps
+            when (t == TError) (EM.Bad "Error: types in an array initializer are not compatible.")
+            return $ TArr (length rexps) t
+    
+    FCall ident rexps -> failure x
 
---     Not rexp -> if inferRExp
---     Comp rexp1 compop rexp2 -> failure x
---     Arith rexp1 arithop rexp2 -> failure x
---     Sign signop rexp -> failure x
---     RefE lexp -> failure x
---     RLExp lexp -> failure x
---     ArrList rexps -> failure x
---     FCall ident rexps -> failure x
---     PredR pread -> return $ tctypeOf literal
---     Lit literal -> return $ tctypeOf literal
+    PredR pread -> return $ tctypeOf pread
+    Lit literal -> return $ tctypeOf literal
