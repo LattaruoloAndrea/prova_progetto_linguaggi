9b7c7cb7dc23935d3133a26d5d49c4a46e9deebc
VashTheStampede96 <brian.riccardi@gmail.com>
2020-05-20 22:38:55 +0200

    fixed compatibility char-string

--- src/haskell/TCType.hs
+++ src/haskell/TCType.hs
@@ -1,6 +1,7 @@
 module TCType where
 
 import AbsChapel (Intent)
+import Control.Monad (liftM2)
 
 -- Type system
 data TCType 
@@ -37,6 +38,8 @@ TInt `compare'` TChar    = TGreater
 TReal `compare'` TInt   = TGreater
 TReal `compare'` TChar  = TGreater
 
+TString `compare'` TChar = TGreater
+
 TError `compare'` TError = TEqual
 _ `compare'` TError      = TLess
 TError `compare'` _      = TGreater
@@ -53,9 +56,16 @@ x `compare'` y = if x == y
 
 
 -- Name from lattice-theory (perché fa figo ovviamente)
+-- If types are comparable, then return the max
+-- else TError
 supremum :: TCType -> TCType -> TCType
 supremum x y
     | x `compare'` y == TLess       = y
     | x `compare'` y == TEqual      = x
     | x `compare'` y == TGreater    = x
     | otherwise                     = TError
+
+
+-- Monadic version for folding
+supremumM :: (Monad m) => m TCType -> m TCType -> m TCType
+supremumM = liftM2 supremum
