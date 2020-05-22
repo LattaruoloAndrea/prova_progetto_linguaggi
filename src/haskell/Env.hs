569e8f1428f2a7277bb286fafe4859923e8f41a3
VashTheStampede96 <brian.riccardi@gmail.com>
2020-05-22 17:33:12 +0200

    lookType Id -> Env -> Err TCType

--- src/haskell/Env.hs
+++ src/haskell/Env.hs
@@ -3,6 +3,9 @@ module Env where
 import qualified Data.Map.Lazy as M
 import AbsChapel
 import TCType
+import qualified ErrM as EM
+import Data.Maybe
+import Control.Monad (join)
 
 type Id = String
 type Env = [Context]
@@ -12,3 +15,26 @@ data Entry
     = Var Loc TCType
     | Const Loc TCType Literal
     | Fun Loc TCType
+
+
+-- Entry have TCType
+instance TCTypeable Entry where
+    tctypeOf x = case x of
+        Var _ t     -> t
+        Const _ t _ -> t
+        Fun _ t     -> t
+
+
+-- Take Just the deepest entry mapped from id (if it exists), otherwise Nothing
+lookEntry :: Id -> Env -> Maybe Entry
+lookEntry id env = join $ head' $ dropWhile isNothing $ map (M.lookup id) env
+
+head' :: [a] -> Maybe a
+head' []        = Nothing
+head' (x:xs)    = Just x
+
+-- Take the Ok TCType from the deepest entry mapped from id (if it exists), otherwise Bad
+lookType :: Id -> Env -> EM.Err TCType
+lookType id env = case lookEntry id env of
+    Nothing -> EM.Bad "Variable not declared."
+    Just x  -> EM.Ok $ tctypeOf x
