f816054a5c02e986868168be75b2cfddaa4828da
VashTheStampede96 <brian.riccardi@gmail.com>
2020-05-22 17:34:09 +0200

    First draft for inferRExp, must improve error messages

--- src/haskell/TypeChecker.hs
+++ src/haskell/TypeChecker.hs
@@ -5,6 +5,7 @@ import TCType
 import TCInstances
 import Locatable
 import Env
+import Control.Monad (unless, when)
 import qualified ErrM as EM
 
 import Control.Monad (when)
@@ -18,15 +19,43 @@ failure x = EM.Bad $ "Undefined case: " ++ show x
 
 
 inferRExp :: Env -> RExp -> EM.Err TCType
-inferRExp env rexp = let x = "Dummy" in case rexp of
-    Or _ rexp1 rexp2 -> failure x
-    And _ rexp1 rexp2 -> failure x
-    Not _ rexp -> failure x
-    Comp _ rexp1 compop rexp2 -> failure x
-    Arith _ rexp1 arithop rexp2 -> failure x
-    Sign _ signop rexp -> failure x
-    RefE _ lexp -> failure x
-    RLExp _ lexp -> failure x
+inferRExp env rexp = case rexp of -- Let to be deleted when all cases are completed
+    Or _ rexp1 rexp2 -> do
+        t1 <- inferRExp env rexp1
+        t2 <- inferRExp env rexp2
+        unless (t1 == TBool && t2 == TBool) (EM.Bad "Operands are not of type bool in a OR expression.")
+        return TBool
+
+    And _ rexp1 rexp2 -> do
+        t1 <- inferRExp env rexp1
+        t2 <- inferRExp env rexp2
+        unless (t1 == TBool && t2 == TBool) (EM.Bad "Operands are not of type bool in a AND expression.")
+        return TBool
+    
+    Not _ rexp -> do
+        t <- inferRExp env rexp
+        unless (t == TBool) (EM.Bad "Operand is not of type bool in a NOT expression.")
+        return TBool
+        
+    Comp _ rexp1 compop rexp2 -> do
+        t1 <- inferRExp env rexp1
+        t2 <- inferRExp env rexp2
+        let t = supremum t1 t2
+        when (t == TError) (EM.Bad "Operands are not compatible in a COMPARISON expression.")
+        return TBool
+
+    Arith _ rexp1 arithop rexp2 -> do
+        t1 <- inferRExp env rexp1
+        t2 <- inferRExp env rexp2
+        let t = supremum t1 t2
+        when (t == TError) (EM.Bad "Operands are not compatible in an ARITHMETIC expression.")
+        return t
+
+    Sign _ signop rexp -> inferRExp env rexp
+
+    -- RefE _ lexp -> failure x    @TODO: infer for LEXP
+
+    -- RLExp _ lexp -> failure x   @TODO: infer for LEXP
     
     ArrList _ rexps -> case rexps of
         [] -> EM.Bad "Error: empty array initializer."
@@ -35,7 +64,10 @@ inferRExp env rexp = let x = "Dummy" in case rexp of
             when (t == TError) (EM.Bad "Error: types in an array initializer are not compatible.")
             return $ TArr (length rexps) t
     
-    FCall _ ident rexps -> failure x
+    FCall _ ident rexps -> lookType (idName ident) env
 
     PredR _ pread -> return $ tctypeOf pread
+
     Lit _ literal -> return $ tctypeOf literal
+
+    _             -> EM.Bad "INFER OF A NON-COVERED REXP."
