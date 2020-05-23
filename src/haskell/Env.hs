4fe5099341aa8374c7e9bb5f014be85b59adf96a
VashTheStampede96 <brian.riccardi@gmail.com>
2020-05-23 19:15:55 +0200

    added instance Show, added functions to push loop scopes

--- src/haskell/Env.hs
+++ src/haskell/Env.hs
@@ -6,6 +6,7 @@ import TCType
 import qualified ErrM as EM
 import Data.Maybe
 import Control.Monad (join)
+import TCInstances
 
 type Id = String
 type Env = [Context]
@@ -13,12 +14,14 @@ data Context = Context {
       entryMap  :: M.Map Id Entry
     , returns   :: TCType
     , inWhile   :: Bool
-}  
+    , inFor     :: Bool
+} deriving (Show)
 
 data Entry
     = Var Loc TCType
     | Const Loc TCType Literal
     | Fun Loc TCType
+    deriving (Show)
 
 
 -- Entry have TCType
@@ -40,14 +43,41 @@ head' (x:xs)    = Just x
 -- Take the Ok TCType from the deepest entry mapped from id (if it exists), otherwise Bad
 lookType :: Id -> Env -> EM.Err TCType
 lookType id env = case lookEntry id env of
-    Nothing -> EM.Bad "Variable not declared."
-    Just x  -> EM.Ok $ tctypeOf x
+    Nothing -> EM.Bad "Name not declared."
+    Just x  -> return $ tctypeOf x
+
+-- Take the Ok (Fun ..) from the deepest entry mapped from id (if it exists), otherwise Bad
+lookFun :: Id -> Env -> EM.Err Entry
+lookFun id env = case lookEntry id env of
+    Just f@(Fun _ _) -> return f
+    _                -> EM.Bad "Function not declared."
+
+
+-- Add constant to the deepest context given Ident and value
+-- (WARNING) If the name is already the context the entry will be overwritten:
+--           check the existence first!
+makeConst :: Env -> Ident -> Literal -> Env
+makeConst env@(c:cs) (Ident l n) lit =
+    let m = M.insert n (Const l (tctypeOf lit) lit) (entryMap c)
+    in (Context m (returns c) (inWhile c) (inFor c)) : cs
 
 
 -- Push an empty context on top of the stack
 pushContext :: Env -> Env
-pushContext (c:cs) = (Context mempty (returns c) (inWhile c)) : (c : cs)
+pushContext env@(c:cs) = (Context mempty (returns c) (inWhile c) (inFor c)) : env
+
+-- Push an empty context on top of the stack for an unbounded iteration (inWhile = True)
+pushWhile :: Env -> Env
+pushWhile env@(c:cs) = (Context mempty (returns c) True (inFor c)) : env
+
+-- Push an emmpty context on top of the stack for a bounded iteration (inWhile = False, inFor = True)
+pushFor :: Env -> Env
+pushFor env@(c:cs) = (Context mempty (returns c) False True) : env
 
 -- Pop context on top of the stack
 popContext :: Env -> Env
-popContext (c:cs) = cs 
+popContext (c:cs) = cs
+
+-- Add a for-loop counter (as constant with dummy int value) to the deepest context given Ident
+makeForCounter :: Env -> Ident -> Env
+makeForCounter env@(c:cs) ident = makeConst env ident (LInt 0)
