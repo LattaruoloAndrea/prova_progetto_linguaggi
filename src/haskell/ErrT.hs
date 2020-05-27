535da8d265c549b18e7964024b43758690be0882
VashTheStampede96 <brian.riccardi@gmail.com>
2020-05-27 18:58:28 +0200

    Error concat

--- src/haskell/ErrT.hs
+++ src/haskell/ErrT.hs
@@ -17,11 +17,24 @@ data ErrT a = OkT a | BadT a Log
   deriving (Read, Show, Eq, Ord)
 
 
+-- Transform an ErrT to a BNFC-Err
+fromErrT :: ErrT a -> Err a
+fromErrT (OkT a) = Ok a
+fromErrT (BadT _ s) = Bad . unlines $ DL.toList s
+
 -- Transform a BNFC-Err in an ErrT
 -- a 'basic' value is provided for BadT
 toErrT :: a -> Err a -> ErrT a
 toErrT _ (Ok a)  = OkT a
 toErrT a (Bad s) = BadT a $ DL.fromList [s]
+
+-- Transform a BNFC-Err in an ErrT
+-- a Err value is provided for BadT
+-- also, a 'basic' value is provided in case the first one is Bad
+toErrTM :: a -> Err a -> Err a -> ErrT a
+toErrTM _ _ (Ok a)    = OkT a
+toErrTM _ (Ok a') bad = toErrT a' bad
+toErrTM a bad1 bad2   = toErrT a bad2
 
 instance Monad ErrT where
   return         = OkT
--- src/haskell/TypeChecker.hs
+++ src/haskell/TypeChecker.hs
@@ -35,10 +35,10 @@ predList = [
 startEnv :: Env
 startEnv = [Context (M.fromList predList) TVoid False False False]
 
-typeCheck :: Program -> EM.Err Env
+typeCheck :: Program -> ET.ErrT Env
 typeCheck = checkProgram startEnv
 
-checkProgram :: Env -> Program -> EM.Err Env
+checkProgram :: Env -> Program -> ET.ErrT Env
 checkProgram env (Prog decls) = do
     env1 <- foldM loadFunction env decls
     foldM checkDecl env1 decls
@@ -138,7 +138,7 @@ checkLExpError = checkExpError inferLExp
 
 checkStm :: Env -> Stm -> EM.Err Env -- To be changed to ET.Err ()
 checkStm env stm = case stm of
-    StmBlock block -> checkBlock env block
+    StmBlock block -> ET.fromErrT $ checkBlock env block
 
     StmCall ident rs -> do
         f <- lookFun ident env -- @TODO: check that arguments are ok with function definition
@@ -212,14 +212,15 @@ checkRange env rng = do
     unless (te `subtypeOf` TInt) $ errorRangeEnd (locOf rng) (end rng) te
     return env
 
-checkDecl :: Env -> Decl -> EM.Err Env
+checkDecl :: Env -> Decl -> ET.ErrT Env
 checkDecl env decl = case decl of
     FDecl id forms it ty blk -> do
         let params  = map formToParam forms             -- create Params from Forms
-        let entries = map paramToEntry params           -- create EnvEntries from Params
-        let env1    = pushFun env (tctypeOf ty) it
-        unless (it==In || it==Ref) (EM.Bad "Return intent must be In or Ref.")
-        env2 <- foldM makeEntry env1 $ zip [identFromParam p | p <- params] entries  -- fold the entry insertion given the list (id, entry)
+            entries = map paramToEntry params           -- create EnvEntries from Params
+            env1    = pushFun env (tctypeOf ty) it
+        unless (it==In || it==Ref) $ ET.toErrT () $ errorReturnIntent (locOf id) it
+        let makeEntry' = \e p -> ET.toErrT e $ makeEntry e p -- return the old env if something goes wrong
+        env2 <- foldM makeEntry' env1 $ zip [identFromParam p | p <- params] entries  -- fold the entry insertion given the list (id, entry)
         env3 <- checkBlock env2 blk
         return $ popContext env3
 
@@ -230,43 +231,57 @@ checkDecl env decl = case decl of
         foldM checkVDecl env vs
 
 
-checkCDecl :: Env -> CDecl -> EM.Err Env
-checkCDecl env c@(CDecl id t r) = do
-    tr <- inferRExp env r                 -- Error purpose: check that r is well-typed
-    unless (tr `subtypeOf` (tctypeOf t)) (EM.Bad $ "Error: type mismatch in a CONST DECLARATION at line " ++ (show $ locOf id) ++ ".")
-    case constexpr env r of
-        Nothing -> (EM.Bad "Error: the expression is not a const-expression in a CONST DECLARATION.")
-        Just x  -> do
 
+-- Extends the environment with a new compile-time constant (if possibile)
+--  * If no duplicate declaration occures and type is correct and initializer is a compile-time constant, OkT (env++constant)
+--  * Otherwise, BadT env
+checkCDecl :: Env -> CDecl -> ET.ErrT Env
+checkCDecl env c@(CDecl id t r) = ET.toErrT env $ let tc = tctypeOf t in do
+    tr <- inferRExp env r                 -- Error purpose: check that r is semantically correct
+    unless (tr `subtypeOf` tc) $ errorConstTypeMismatch id tc tr
+    case constexpr env r of
+        Nothing -> errorNotConst id r
+        Just x  -> do
             makeConst env id x
 
 
-checkVDecl :: Env -> VDecl -> EM.Err Env
+-- Extends the environment with a new variable (if possible)
+--  * If no duplicate declaration occures and type of initialization is correct, OkT (env++initialized) 
+--  * If no duplicate declaration occures and type of initialization is incorrect, BadT (env++not_initialized)
+--  * If duplicate declaration occures, BadT env
+checkVDecl :: Env -> VDecl -> ET.ErrT Env
 checkVDecl env v = case v of
-    Solo id t   -> makeMutable env id (tctypeOf t)
+    Solo id t   -> ET.toErrT env $ makeMutable env id (tctypeOf t)
 
-    Init id t r -> do
-        let tc = tctypeOf t
+    Init id t r -> let tc = tctypeOf t in ET.toErrTM env (makeMutable env id tc) $ do
         tr <- inferRExp env r
-        unless (tr `subtypeOf` tc) $ badLoc (locOf id) "Type mismatch in a VAR DECLARATION"
+        unless (tr `subtypeOf` tc) $ errorVarTypeMismatch id tc tr
         makeMutable env id tc
 
 
 -- Insert function in the scope (to be used once we enter in a new block to permit mutual-recursion)
-loadFunction :: Env -> Decl -> EM.Err Env
-loadFunction env d = case d of
+--  * If no duplicate declaration occures, OkT (env++function)
+--  * Otherwise, BadT env
+loadFunction :: Env -> Decl -> ET.ErrT Env
+loadFunction env d = ET.toErrT env $ case d of
     FDecl id forms it ty _  ->
         let params = map formToParam forms      -- formToParam is in Env.hs
         in makeFun env id params it $ tctypeOf ty
     
     _                       -> return env
 
-checkBlock :: Env -> Block -> EM.Err Env
+
+-- * Push a new empty context
+-- * Load all function names (to allow mutual-recursion)
+-- * Check all declarations
+-- * Check all statements
+-- * Pop the new context
+checkBlock :: Env -> Block -> ET.ErrT Env
 checkBlock env block = do
     let env1 = pushContext env
     env2 <- foldM loadFunction env1 (decls block)
     env3 <- foldM checkDecl env2 (decls block)
-    env4 <- foldM checkStm  env3 (stms  block)
+    env4 <- ET.toErrT env3 $ foldM checkStm env3 (stms block)
     return $ popContext env4
 
 
@@ -276,19 +291,19 @@ checkBlock env block = do
 checkJmp :: Env -> Jump -> EM.Err () -- To be changed to ET.Err ()
 checkJmp env@(c:cs) jmp = case jmp of
     Return _    -> do
-        when (inFor c || inWhile c) (EM.Bad "Cannot return inside a loop.")
-        unless (returns c == TVoid) (EM.Bad "cannot return a type different from void in a PROCEDURE")
+        when (inFor c || inWhile c) $ errorReturnLoop $ locOf jmp
+        unless (returns c == TVoid) $ errorReturnProcedure (locOf jmp) $ returns c
 
     ReturnE _ r -> do
         t <- checkRExpError env r
         let t' = returns c
-        when (inFor c || inWhile c) (EM.Bad "Cannot return inside a loop.")
-        unless (t `subtypeOf` t') (EM.Bad ("the return type should be " ++ (show t')))
+        when (inFor c || inWhile c) $ errorReturnLoop $ locOf jmp
+        unless (t `subtypeOf` t') $ errorReturnTypeMismatch (locOf jmp) t t'
 
     Break _     -> do
-        when (inFor c) (EM.Bad "Cannot use break inside a for statement.")
-        unless (inWhile c) (EM.Bad "cannot use break outside a (do-)while statement.")
+        when (inFor c) $ errorBreakFor $ locOf jmp
+        unless (inWhile c) $ errorBreakOutside $ locOf jmp
     
     Continue _  -> do
-        when (inFor c) (EM.Bad "Cannot use continue inside a for-statement.")
-        unless (inWhile c) (EM.Bad "cannot use continue outside a (do-)while statement.")
+        when (inFor c) $ errorContinueFor $ locOf jmp
+        unless (inWhile c) $ errorContinueOutside $ locOf jmp
