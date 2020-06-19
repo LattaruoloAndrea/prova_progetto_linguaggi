db79caeb16720d66f7194fea31ec1a3833c97741
VashTheStampede96 <brian.riccardi@gmail.com>
2020-06-19 15:54:25 +0200

    fixed bool compatibility

--- src/haskell/TCType.hs
+++ src/haskell/TCType.hs
@@ -45,15 +45,19 @@ data TOrdering = TLess | TEqual | TGreater | TNotComparable
     deriving (Eq, Show)
 
 compare' :: TCType -> TCType -> TOrdering
+TBool `compare'` TChar = TLess
 TBool `compare'` TInt = TLess
+TBool `compare'` TReal = TLess
 
 TChar `compare'` TInt    = TLess
 TChar `compare'` TReal  = TLess
+TChar `compare'` TBool = TGreater
 
-TInt `compare'` TBool = TGreater
 TInt `compare'` TReal   = TLess
+TInt `compare'` TBool = TGreater
 TInt `compare'` TChar    = TGreater
 
+TReal `compare'` TBool = TGreater
 TReal `compare'` TInt   = TGreater
 TReal `compare'` TChar  = TGreater
 
